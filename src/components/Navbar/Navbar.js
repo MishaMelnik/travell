543806d75d8be8548d1f174e435567e332879fe6
misha <misha.melnik2016@gmail.com>
2021-11-02 11:19:19 +0200

    create adaptive for phone and iPad

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import'./Navbar.css'
+import React, {useState} from 'react';
+import './Navbar.css'
 import {FiMenu, FiX} from "react-icons/fi";
 
 // const navbarLinks =[{ url:"/home", title:"Home"}];
 
 const Navbar = ({navbarLinks}) => {
+    const [menuClicked, setMenuClicked] = useState(false);
+
+    const toggleMenuClick = () => {
+        setMenuClicked(!menuClicked);
+    }
     return (
         <nav className="navbar">
             <span className="navbar_logo">travell</span>
-            <ul className="navbar_list">
-                {navbarLinks.map((item) =>{
-                return(
-                    <li className="navbar_item" key={item.title}>
-                        <a className="navbar_link" href={item.url}>
-                            {item.title}
-                        </a>
-                    </li>
-                );
-            })}
+            {menuClicked ? (
+                <FiMenu size={25} className="navbar_menu" onClick={toggleMenuClick}/>
+            ) : (
+                <FiX size={25} className="navbar_menu" onClick={toggleMenuClick}/>
+                )}
+            <ul className={menuClicked ? "navbar_list" : "navbar_list navbar_list--active"}>
+                {navbarLinks.map((item) => {
+                    return (
+                        <li className="navbar_item" key={item.title}>
+                            <a className="navbar_link" href={item.url}>
+                                {item.title}
+                            </a>
+                        </li>
+                    );
+                })}
             </ul>
         </nav>
     );
